fffc33473a1400d44aa7f0faa8e6fb44ad4a2219
Matheus Nunes <devmatheusnunesdebarros@gmail.com>
2024-05-07 16:09:25 -0300

    Personagens

--- src/script/main.js
+++ src/script/main.js
@@ -1,9 +1,41 @@
 const getPersonagem = document.querySelectorAll('.personagens__nav__lista__item')
-const selecionarPersonagem = document.getElementById('selecionar--personagem')
+const selecionarNome = document.querySelector('.selecionado__dados__name')
+const respiracao = document.getElementById('Respiracao')
+const categoria = document.getElementById('Categoria')
+const personagensNomes = [
+    {tanjiro: 'Tanjiro Kamado', respiracao: 'Sol e Água'},
+    {nezuko: 'Nezuko Kamado', tipo: 'Humana/Oni'},
+    {zenitsu: 'Zenitsu Agatsuma', respiracao: 'Trovão'},
+    {inosuke: 'Inosuke Hashibira', respiracao: 'Fera'},
+    {kanao: 'Kanao Tsuyuri', respiracao: 'Flores'},
+    {yoriichi:'Yoriichi Tsugikuni', respiracao: 'Sol'},
+    {gyu: 'Giyu Tomioka', respiracao:'Água'},
+    {shinobu: 'Shinobu Kochō', respiracao: 'Inseto'},
+    {kyojuro: 'Kyōjurō Rengoku', respiracao: 'Fogo'},
+    {mitsuri: 'Mitsuri Kanroji', respiracao: 'Amor'},
+    {obanai: 'Obanai Iguro', respiracao: 'Serpente'},
+    {sanemi: 'Sanemi Shinazugawa', respiracao: 'Vento'},
+    {gyomei: 'Gyomei Himejima', respiracao: 'Pedra'},
+    {muichiro:'Muichiro Tokito', respiracao: 'Névoa'},
+    {tengen: 'Tengen Uzui', respiracao: 'Som'},
+]
+const mainBackground = document.querySelector('.main')
 getPersonagem.forEach((p)=>{
     p.addEventListener('click', (e)=>{
         const dataName = p.dataset.personagens
-        selecionarPersonagem.src = `src/assets/personagens_exibicao/${dataName}.png`
+        mainBackground.style.backgroundImage = `url(../../src/assets/personagens-background/${dataName}.jpg)`;
+        personagensNomes.forEach((personagem)=>{
+            if(personagem[dataName]){
+                if(dataName == 'nezuko'){
+                    selecionarNome.innerHTML = personagem[dataName]
+                    categoria.innerText = 'TIPO: '
+                    respiracao.innerHTML = personagem['tipo']
+                }else{
+                    categoria.innerText = 'RESPIRAÇÃO: '
+                    selecionarNome.innerHTML = personagem[dataName]
+                    respiracao.innerHTML =  personagem['respiracao']
+                }
+            }
+        })
     })
 })
-console.log('oi')
